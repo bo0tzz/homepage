c47611768604a8ca739fa69acbd8eb135e09b6f9
bo0tzz <git@bo0tzz.me>
2023-10-10 16:10:52 +0200

    Remove dbg calls

--- lib/homepage/hackernews.ex
+++ lib/homepage/hackernews.ex
@@ -25,7 +25,7 @@ defmodule Homepage.Hackernews do
           {base, query}
       end
 
-    param = URI.encode_query(query, :rfc3986) |> dbg()
+    param = URI.encode_query(query, :rfc3986)
 
     %URI{
       scheme: "https",
@@ -44,6 +44,5 @@ defmodule Homepage.Hackernews do
     |> Floki.find("title")
     |> Floki.text()
     |> String.trim()
-    |> dbg()
   end
 end
